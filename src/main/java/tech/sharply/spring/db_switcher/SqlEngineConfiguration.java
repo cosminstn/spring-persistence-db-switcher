afe243cac6ebb7733eb80b24f6faf217b08b929d
Cosmin Stoian <cosmyn9708@gmail.com>
2021-02-13 09:43:17 +0200

    Code cleanup.

--- src/main/java/tech/sharply/spring/db_switcher/SqlEngineConfiguration.java
+++ src/main/java/tech/sharply/spring/db_switcher/SqlEngineConfiguration.java
@@ -1,25 +1,15 @@
 package tech.sharply.spring.db_switcher;
 
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.datasource", value = "url")
@@ -30,9 +20,4 @@ import java.util.Properties;
 @Import({DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableJpaRepositories
 public class SqlEngineConfiguration {
-
-	private static final Logger log = LoggerFactory.getLogger(SqlEngineConfiguration.class);
-
 }
-
-
